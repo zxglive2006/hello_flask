41b218d109bc50fef7bf8a487097729a16c5556c
ZhangXiaogui <zxglive2006@126.com>
2019-05-15 16:14:06 +0800

    增加代码和说明

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ## 入门文档：
 #### http://flask.pocoo.org/docs/1.0/quickstart/#quickstart
 #### http://flask.pocoo.org/docs/1.0/tutorial/
+
+## 启动命令：
+#### set FLASK_APP=hello.py
+#### flask run --host=0.0.0.0
--- hello.py
+++ hello.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello, World'
